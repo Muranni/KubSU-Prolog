3b892443b43422f4b30cfae50231bbd1e6656e50
Mikhil Baschenko <muranni587@gmail.com>
2020-06-09 20:56:45 +0300

    IZ2

--- IZ1.pl
+++ IZ1.pl
@@ -3,8 +3,21 @@
 Минимальное число с избытком – это 12. 
 Найдите количество чисел, меньшее 20000, которые нельзя представить в виде суммы двух чисел с избытком.
 12, 18, 20, ...*/
+gottaCatchThemAll :- %Основной предикат, задавать вопрос ему
+    gottaCatchThemAll(25, 23, 20000). /*Рассматриваем числа от 25 т.к. минимальное число, которое
+									    можно представить в виде суммы 2 избыточных чисел - 24,*/
+gottaCatchThemAll(C, Izb, Lim)        /*следовательно имеем 23 числа (1..23), удовлетворяющие заданию*/
+    C<Lim,
+    C1 is C div 2,
+    checkSum(C, C1, Izb, Lim),
+    !.
 
-checkIzb(C) :-
+gottaCatchThemAll(Lim, Izb, _) :-
+    write("Количество чисел, удовлетворяющих условию = "),
+    write(Izb),
+    !.
+
+checkIzb(C) :- %Проверка числа на избыточность
     checkIzb(C, 1, 0).
 
 checkIzb(C, D, S) :-
@@ -24,35 +37,18 @@ checkIzb(C, _, S) :-
     S>C,
     !.
 
-gottaCatchThemAll :-
-    gottaCatchThemAll(24, 23).
-
-gottaCatchThemAll(C, Izb) :-
-    C1 is C div 2,
-    findFirst(C, C1),
-    Cn is C+1,
-    gottaCatchThemAll(Cn, Izb).
-
-gottaCatchThemAll(_, 19999) :-
-    !.
-
-gottaCatchThemAll(26, _) :-
-    !.
-    
-findFirst(C, C1) :-
+checkSum(C, C1, Izb, Lim) :- %Проверка числа, можно ли его представить в виде суммы 2 избыточных чисел
     C1>=12,
     checkIzb(C1),
-    !,
     C2 is C-C1,
     checkIzb(C2),
-    !.
-
-findFirst(C, C1) :-
-    C1>12,
-    !,
-    C11 is C1-1,
-    findFirst(C, C11).
-
-findFirst(C, _) :-
     Cn is C+1,
-    gottaCatchThemAll(Cn, Izb).
+    gottaCatchThemAll(Cn, Izb, Lim);
+    C1>12,
+    C11 is C1-1,
+    checkSum(C, C11, Izb, Lim).
+
+checkSum(C, _, Izb, Lim) :-
+    Cn is C+1,
+    Izb2 is Izb+1,
+    gottaCatchThemAll(Cn, Izb2, Lim).
